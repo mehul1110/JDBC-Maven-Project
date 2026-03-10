6d0541cec350679a035258f5460fb3ee0956076e
mehul1110 <mehulshra1110@gmail.com>
2026-03-11 03:20:41 +0530

    UC2: retrieve all employee payroll data from DB

--- src/main/java/com/bridgelabz/EmployeePayrollService.java
+++ src/main/java/com/bridgelabz/EmployeePayrollService.java
@@ -1,14 +1,16 @@
-package com.payroll;
+package com.bridgelabz;
 
 import java.sql.*;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 public class EmployeePayrollService {
 
     private static final String JDBC_URL =
-            "jdbc:mysql://localhost:3306/payroll_service?useSSL=false";
+            "jdbc:mysql://localhost:3306/payroll_service?useSSL=false&allowPublicKeyRetrieval=true";;
     private static final String USERNAME = "root";
-    private static final String PASSWORD = "123456";
-
+    private static final String PASSWORD = "123456"; // change this
 
     public Connection getConnection() throws SQLException {
         Connection con = DriverManager.getConnection(JDBC_URL, USERNAME, PASSWORD);
@@ -16,12 +18,32 @@ public class EmployeePayrollService {
         return con;
     }
 
-    public static void main(String[] args) {
-        EmployeePayrollService service = new EmployeePayrollService();
-        try {
-            service.getConnection();
+    public List<EmployeePayrollData> getEmployeePayrollData() {
+        List<EmployeePayrollData> employeePayrollList = new ArrayList<>();
+        String sql = "SELECT * FROM employee_payroll";
+
+        try (Connection con = getConnection();
+             Statement stmt = con.createStatement();
+             ResultSet rs = stmt.executeQuery(sql)) {
+
+            while (rs.next()) {
+                int id = rs.getInt("id");
+                String name = rs.getString("name");
+                double salary = rs.getDouble("salary");
+                LocalDate startDate = rs.getDate("start").toLocalDate();
+                employeePayrollList.add(new EmployeePayrollData(id, name, salary, startDate));
+            }
+
         } catch (SQLException e) {
             e.printStackTrace();
         }
+        return employeePayrollList;
+    }
+
+    public static void main(String[] args) {
+        EmployeePayrollService service = new EmployeePayrollService();
+        List<EmployeePayrollData> employees = service.getEmployeePayrollData();
+        System.out.println("Employee Payroll Data: ");
+        employees.forEach(System.out::println);
     }
 }
